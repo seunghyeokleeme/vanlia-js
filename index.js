4fdf850102aeab9bd59398779e1f3a1e310c70ee
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-07-27 22:22:09 +0900

    calculator example

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const calculator = {
+const cal = {
   plus: function(a, b) {
     return a + b;
+  },
+  minus: function(a, b) {
+    return a - b;
+  },
+  multiply: function(a, b) {
+    return a * b;
+  },
+  devide: function(a, b) {
+    return a / b;
   }
 };
+const plus = cal.plus(5, 5);
+const minus = cal.minus(5, 5);
+const multiply = cal.multiply(5, 5);
+const devide = cal.devide(5, 5);
 
-//console.log(greetDevinan);
-const plus = calculator.plus(5, 5);
-console.log(plus);
+console.log(`${plus}, ${minus}, ${multiply}, ${devide}`);
