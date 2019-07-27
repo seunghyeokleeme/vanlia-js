dd272c8cf1737360bd9534d3bb140f5b19312a38
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-07-27 18:04:35 +0900

    js study point

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 const what = "Devinan";
 console.log(what);
 */
-// Boolean ture is 1 ---- false is 0
+/* Boolean ture is 1 ---- false is 0
 const wat = ture;
+*/
+// for comments
+/* for multi-ilne comments */
+/*
+변수는 되도록 const 사용할것. let 이나 var 말고
+String (" "): 텍스트를 묶어줘
+Boolean: true = 1 / false = 0
+Number: 말 그대로 숫자
+Float: float하는 숫자. 소수점이 있는 경우?
+*/
