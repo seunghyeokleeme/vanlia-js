139491bdfc7a7cb33da1073c357975af35bb9730
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-07-27 21:35:32 +0900

    console.log() is definde why??

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sayHello(name, age) {
-  console.log("Hello!", name, " you have", age, "years of age.");
+  console.log(`Hello! ${name} you have ${age} years of age.`);
 }
 
-sayHello("Devinan", 20);
+const greetDevinan = sayHello("Devinan", 20);
+
+console.log(greetDevinan);
+
+/*
+greetDevinan 은 sayHello의 실행된 결과값이다.(반환)
+but.sayHello는 반환을 안해서 정의 가되지않는다.
+*/
