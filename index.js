8f6b9ee951b01a0b2af112b26e12e0eff1c9a935
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-08-01 13:29:41 +0900

    d

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ const CLICKED_CLASS = "clicked";
 function handleClick() {
   const currentClass = title.className;
   if (currentClass !== CLICKED_CLASS) {
-    title.className = CLICKED_CLASS;
+    title.classList.add(CLICKED_CLASS);
   } else {
-    title.className = "";
+    title.classList.remove(CLICKED_CLASS);
   }
 }
-/* !== 는 같지않는  == 같은
-6번줄에 value 값을 주고 그 className을 가져와서 8번 줄에 그것을 세팅한다.
-같지않으면 className 을 준다.
-*/
 
 function init() {
   title.addEventListener("click", handleClick);
